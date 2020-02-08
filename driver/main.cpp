9f41ef74b31007e89582a83030a9223dbbdecf0b
Usama <usamasattar.3347@gmail.com>
2020-02-08 22:16:11 +0500

    First iteration by Usama

--- driver/main.cpp
+++ driver/main.cpp
@@ -7,7 +7,7 @@ int main()
     // 1. Include the Point2D.h file and create a Point2D object
     // 2. Set the x & y values using the constructor
     // 3. Replace the 800, 600 by the points x & y members
-    Point2D PointObject(640,360);
+    Point2D PointObject(1920,1080);
     sf::RenderWindow window(sf::VideoMode(PointObject.x_,PointObject.y_), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
